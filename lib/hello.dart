a77e5d3e429c56997318a15d82173859bdbc51c8
darshana <darshana.jadhav@finoux.com>
2024-12-02 12:55:58 +0530

    first commit

---
+++ lib/hello.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+
+class Hello extends StatefulWidget {
+  const Hello({super.key});
+
+  @override
+  State<Hello> createState() => _HelloState();
+}
+
+class _HelloState extends State<Hello> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+
+          Center(
+            child: Text("helllloooo",style: TextStyle(
+              color: Colors.red
+            ),),
+          ),
+        ],
+      ),
+    );
+  }
+}
